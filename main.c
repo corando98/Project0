c86c8a177848a1490392412323144ed25bb5cbbd
corando98 <diegocorando@gmail.com>
2021-03-01 20:48:34 -0600

    updated

--- main.c
+++ main.c
@@ -22,10 +22,9 @@ Expected output:
  dog:0
 */
 
-
 //This is a test for Github
 //second test
-//third test 
+//third test
 
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -36,47 +35,48 @@ Expected output:
 #define LENGTH(s) (sizeof(s) / sizeof(*s))
 
 /* Structures */
-typedef struct {
+typedef struct
+{
   char *word;
   int counter;
 } WordCountEntry;
-
 
 int process_stream(WordCountEntry entries[], int entry_count)
 {
   short line_count = 0;
   char buffer[30];
 
-  while (gets(buffer)) {
+  while (fgets(buffer, sizeof(buffer), stdin))
+  {
     if (*buffer == '.')
       break;
+    //lenght of word
+    size_t lenght = strlen(buffer);
+    //wait for newline if so make it 0
+    if (buffer[lenght - 1] == '\n')
+      buffer[--lenght] = 0;
     /* Compare against each entry */
-    int i = 0;
-    while (i < entry_count) {
-      if (!strcmp(entries[i].word, buffer))
-        entries[i].counter++;
-      i++;
-    }
-    line_count++;
+    if (!strcmp(entries[line_count].word, buffer))
+      entries[line_count].counter++; //If found match increase count
+    if (++line_count == entry_count)
+      break;
   }
   return line_count;
 }
 
-
 void print_result(WordCountEntry entries[], int entry_count)
 {
-  printf("Result:\n");
-  while (entry_count-- > 0) {
-    printf("%s:%d\n", entries->word, entries->counter);
+  printf(stdout, "\nResult:\n");
+  for (int i = 0; i < entry_count; i++)
+  {
+    fprintf("%s:%d\n", entries[i].word, entries[i].counter);
   }
 }
 
-
 void printHelp(const char *name)
 {
-  printf("usage: %s [-h] <word1> ... <wordN>\n", name);
+  printf("usage: %s [-h] [-f FILENAME] <word1> ... <wordN>\n", name);
 }
-
 
 int main(int argc, char **argv)
 {
@@ -86,40 +86,48 @@ int main(int argc, char **argv)
   int entryCount = 0;
 
   /* Entry point for the testrunner program */
-  if (argc > 1 && !strcmp(argv[1], "-test")) {
+  if (argc > 1 && !strcmp(argv[1], "-test"))
+  {
     run_smp0_tests(argc - 1, argv + 1);
     return EXIT_SUCCESS;
   }
 
-  while (*argv != NULL) {
-    if (**argv == '-') {
-
-      switch ((*argv)[1]) {
-        case 'h':
-          printHelp(prog_name);
-        default:
-          printf("%s: Invalid option %s. Use -h for help.\n",
-                 prog_name, *argv);
-      }
-    } else {
-      if (entryCount < LENGTH(entries)) {
-        entries[entryCount].word = *argv;
-        entries[entryCount++].counter = 0;
+  for (int i = 1; i < argc; i++)
+  {
+    if (*argv[i] == '-')
+    {
+      switch (argv[i][1])
+      {
+      case 'h':
+        printHelp(prog_name);
+        break;
+      case 'f':
+        freopen((*argv)[2], "w", stdout);
+        break;
+      default:
+        fprintf(stderr, "%s: Invalid arguments %s. Use -h for help. \n ", prog_name, *argv);
       }
     }
-    argv++;
-  }
-  if (entryCount == 0) {
-    printf("%s: Please supply at least one word. Use -h for help.\n",
-           prog_name);
-    return EXIT_FAILURE;
-  }
-  if (entryCount == 1) {
-    printf("Looking for a single word\n");
-  } else {
-    printf("Looking for %d words\n", entryCount);
+    else
+    {
+      entries[entryCount].word = argv[i];
+      entries[entryCount++].counter = 0;
+    }
   }
 
+  if (!entryCount)
+  {
+    fprintf(stderr, "%s: Please suppy atleast one word, Use -h for help. \n", prog_name);
+    return EXIT_FAILURE;
+  }
+  if (entryCount == 1)
+  {
+    printf("Looking for a single word\n");
+  }
+  else
+  {
+    printf("Looking for %d words\n", entryCount);
+  }
   process_stream(entries, entryCount);
   print_result(entries, entryCount);
 
