4836a6284978b67968a7ff22d37706724513f574
Debian <debian@debian10.linuxvmimages.local>
2021-03-01 22:02:07 -0600

    Fixed code

--- main.c
+++ main.c
@@ -22,11 +22,6 @@ Expected output:
  dog:0
 */
 
-//This is a test for Github
-//second test
-//third test
-
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -45,44 +40,46 @@ int process_stream(WordCountEntry entries[], int entry_count)
 {
   short line_count = 0;
   char buffer[30];
+  char *tokTEMP;
 
   while (fgets(buffer, sizeof(buffer), stdin))
   {
+
     if (*buffer == '.')
       break;
-    //lenght of word
-    size_t lenght = strlen(buffer);
-    //wait for newline if so make it 0
-    if (buffer[lenght - 1] == '\n')
-      buffer[--lenght] = 0;
-    /* Compare against each entry */
-    if (!strcmp(entries[line_count].word, buffer))
-      entries[line_count].counter++; //If found match increase count
-    if (++line_count == entry_count)
-      break;
+    tokTEMP = strtok(buffer, " \t\n");
+    while(tokTEMP != NULL){
+      for(int i = 0; i < entry_count; i++) {
+        if (!strcmp(entries[i].word, tokTEMP))
+          entries[i].counter++;
+      }
+      tokTEMP = strtok(NULL, " \t\n");
+    }
+    line_count++;
   }
   return line_count;
 }
 
 void print_result(WordCountEntry entries[], int entry_count)
 {
-  printf(stdout, "\nResult:\n");
-  for (int i = 0; i < entry_count; i++)
+  fprintf(stdout, "Result:\n");
+  int index = 0;
+  for (index = 0; index < entry_count; index++)
   {
-    fprintf("%s:%d\n", entries[i].word, entries[i].counter);
+    fprintf(stdout, "%s:%d\n", entries[index].word, entries[index].counter);
   }
 }
 
 void printHelp(const char *name)
 {
-  printf("usage: %s [-h] [-f FILENAME] <word1> ... <wordN>\n", name);
+  fprintf(stderr, "usage: %s [-h] <word1> ... <wordN>\n", name);
 }
 
 int main(int argc, char **argv)
 {
   const char *prog_name = *argv;
 
-  WordCountEntry entries[5];
+  WordCountEntry *entries = malloc(sizeof(WordCountEntry) * argc);
   int entryCount = 0;
 
   /* Entry point for the testrunner program */
@@ -92,42 +89,52 @@ int main(int argc, char **argv)
     return EXIT_SUCCESS;
   }
 
-  for (int i = 1; i < argc; i++)
+  argv++;
+
+  while (*argv != NULL)
   {
-    if (*argv[i] == '-')
+    if (**argv == '-')
     {
-      switch (argv[i][1])
+      switch ((*argv)[1])
       {
       case 'h':
         printHelp(prog_name);
         break;
       case 'f':
-        freopen((*argv)[2], "w", stdout);
+        //freopen((*argv)[2], "w", stdout);
+        stdout = fopen(*argv + 2, "w");
         break;
       default:
-        fprintf(stderr, "%s: Invalid arguments %s. Use -h for help. \n ", prog_name, *argv);
+        fprintf(stderr, "%s: Invalid option %s. Use -h for help.\n",
+                prog_name, *argv);
       }
     }
     else
     {
-      entries[entryCount].word = argv[i];
-      entries[entryCount++].counter = 0;
-    }
-  }
+      if (entryCount < argc - 1)
+      {
 
-  if (!entryCount)
+        entries[entryCount].word = *argv;
+        entries[entryCount++].counter = 0;
+      }
+    }
+    argv++;
+  }
+  if (entryCount == 0)
   {
-    fprintf(stderr, "%s: Please suppy atleast one word, Use -h for help. \n", prog_name);
+    fprintf(stderr, "%s: Please supply at least one word. Use -h for help.\n",
+            prog_name);
     return EXIT_FAILURE;
   }
   if (entryCount == 1)
   {
-    printf("Looking for a single word\n");
+    fprintf(stdout, "Looking for a single word\n");
   }
   else
   {
-    printf("Looking for %d words\n", entryCount);
+    fprintf(stdout, "Looking for %d words\n", entryCount);
   }
+
   process_stream(entries, entryCount);
   print_result(entries, entryCount);
 
